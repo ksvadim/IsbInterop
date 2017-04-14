19be9114081709298649e571b7119555d74b6a40
ksvadim <ksvadim@mail.ru>
2017-04-14 21:34:09 +0400

    Рефакторинг IsblSystemConstants.

--- src/IsbInterop/IsblSystemConstants/DataConstants/ObjectRequisites/PredefinedEDocumentsRequisites.vb
+++ src/IsbInterop/IsblSystemConstants/DataConstants/ObjectRequisites/PredefinedEDocumentsRequisites.vb
@@ -1,13 +1,19 @@
 ﻿Namespace IsblSystemConstants.DataConstants.ObjectRequisites
+
   ''' <summary>
   ''' Предопределенные реквизиты электронных документов.
   ''' </summary>
   Public NotInheritable Class PredefinedEDocumentsRequisites
-    Private Sub New()
-    End Sub
+
     ''' <summary>
     ''' Наименование электронного документа.
     ''' </summary>
     Public Const SYSREQ_EDOC_NAME As String = "ISBEDocName"
+
+    ''' <summary>
+    ''' Конструктор.
+    ''' </summary>
+    Private Sub New()
+    End Sub
   End Class
-End NameSpace
+End Namespace
